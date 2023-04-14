7bff11444772eaa45bae00df459cb9483f1b50e5
renan.oliveira <renan.oliveira@DSK-JBH9YR2>
2023-04-14 18:02:03 -0300

    Adicionar arquivos de projeto.

---
+++ EstudoTreinamento/MainWindowsVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoTreinamento
+{
+    public class MainWindowsVM: INotifyPropertyChanged{
+
+        public RelayCommand show { get; private set; }
+        public string stringBox { get; set; }
+
+        public MainWindowsVM() { 
+            InitializeCommands();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Notify(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void InitializeCommands()
+        {
+            show = new RelayCommand((object _) =>
+            {
+                Notify("stringBox");
+            });
+        }
+    }
+
+ 
+}
